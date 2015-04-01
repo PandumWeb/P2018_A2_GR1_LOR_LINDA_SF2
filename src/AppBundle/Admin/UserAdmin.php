9f264b7ac47519d925a33781d53d35a7bd983344
PandumWeb <lindalor.web@gmail.com>
2015-04-01 09:50:58 +0200

    tagAdmin + CategoryAdmin + UserAdmin

---
+++ src/AppBundle/Admin/UserAdmin.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class UserAdmin extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('firstname')
+            ->add('lastname')
+           
+
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('firstname')
+            ->add('lastname')
+          
+
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('id')
+            ->add('firstname')
+            ->add('lastname')
+           
+        ;
+    }
+
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('firstname')
+            ->add('lastname')
+
+         ;
+    }
+}
